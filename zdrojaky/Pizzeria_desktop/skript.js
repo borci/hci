0020c2fc091fec0e7e92c3b3493ec90604f7d147
Jozef Knaperek <jknaperek@gmail.com>
2011-10-26 17:23:33 +0200

    Odstranene handlenie klikania na triggery (v desktopovej verzii to nie je potrebne), nastaveny default kurzor

--- zdrojaky/Pizzeria_desktop/skript.js
+++ zdrojaky/Pizzeria_desktop/skript.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 //    ******************** triggery pre vysuvanie panelov *****************
-    $(".left_trigger").click(function(){
-        $(".right_trigger").toggle();
-        $(".left_panel").toggle("fast");
-        $(this).toggleClass("active");
-        return false;
-    });
-                
-    $(".right_trigger").click(function(){
-        $(".left_trigger").toggle();
-        $(".right_panel").toggle("fast");
-        $(this).toggleClass("active");
-        return false;
-    });
+//    $(".left_trigger").click(function(){
+//        $(".right_trigger").toggle();
+//        $(".left_panel").toggle("fast");
+//        $(this).toggleClass("active");
+//        return false;
+//    });
+//                
+//    $(".right_trigger").click(function(){
+//        $(".left_trigger").toggle();
+//        $(".right_panel").toggle("fast");
+//        $(this).toggleClass("active");
+//        return false;
+//    });
             
 //    ******************* kosik ****************************
     $("#infolist").accordion({
