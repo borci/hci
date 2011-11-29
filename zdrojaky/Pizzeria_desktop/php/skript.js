085d8083aea5b03128468d79661f1ea634f7964c
Jozef Knaperek <jknaperek@gmail.com>
2011-11-29 22:56:12 +0100

    nic extra.

--- zdrojaky/Pizzeria_desktop/php/skript.js
+++ zdrojaky/Pizzeria_desktop/php/skript.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
     //    ******************** triggery pre vysuvanie panelov *****************
-    $(".left_trigger").click(function(){
-        $(".right_trigger").toggle();
-        $(".left_panel").toggle("fast");
-        $(this).toggleClass("active");
-        uprav_indexy_sipok();
-        return false;
-    });
                 
-    $(".right_trigger").click(function(){
-        $(".left_trigger").toggle();
-        $(".right_panel").toggle("fast");
-        $(this).toggleClass("active");
-        uprav_indexy_sipok();
-        return false;
-    });
-            
     // inicializacia kosika
     init_kosik();
     
